9626f7a1fb1ec1394cbe092a78986614d16f268b
Sam Barros <samarony.barros@gmail.com>
2020-07-19 16:32:23 +0200

    fix: uses heroku port

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,6 +1,6 @@
 const config = {
   serverUrl: process.env.SERVER_URL,
-  serverPort: process.env.SERVER_PORT,
+  serverPort: process.env.PORT,
   serverDatabase: process.env.SERVER_DB_PROD,
   jwtSecret: process.env.JWT_SECRET,
 }
